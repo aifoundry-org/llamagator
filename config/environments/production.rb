590be52c4bb31557295f4488fd001af7a6599e77
Steven R. Baker <steven@stevenrbaker.com>
2024-06-06 01:07:27 +0200

    This is apparently called LlamaGator.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
-  # config.active_job.queue_name_prefix = "prompt_bot_production"
+  # config.active_job.queue_name_prefix = "llama_gator_production"
 
   config.action_mailer.perform_caching = false
 

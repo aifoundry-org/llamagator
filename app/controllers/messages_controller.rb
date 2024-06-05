14ece4ff6dce58e37d455f35f8e53fa6ff53bb30
Steven R. Baker <steven@stevenrbaker.com>
2024-06-05 21:10:51 +0200

    Don't need generated comments.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,25 +1,20 @@
 class MessagesController < ApplicationController
   before_action :set_message, only: %i[ show edit update destroy ]
 
-  # GET /messages or /messages.json
   def index
     @messages = Message.all
   end
 
-  # GET /messages/1 or /messages/1.json
   def show
   end
 
-  # GET /messages/new
   def new
     @message = Message.new
   end
 
-  # GET /messages/1/edit
   def edit
   end
 
-  # POST /messages or /messages.json
   def create
     @message = Message.new(message_params)
 
@@ -34,7 +29,6 @@ class MessagesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /messages/1 or /messages/1.json
   def update
     respond_to do |format|
       if @message.update(message_params)
@@ -47,7 +41,6 @@ class MessagesController < ApplicationController
     end
   end
 
-  # DELETE /messages/1 or /messages/1.json
   def destroy
     @message.destroy!
 
@@ -58,12 +51,10 @@ class MessagesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_message
       @message = Message.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def message_params
       params.require(:message).permit(:body)
     end

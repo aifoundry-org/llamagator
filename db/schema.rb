01c24c0ade6d764bdac9e21cd8f8a06fd7a1ae64
Steven R. Baker <steven@stevenrbaker.com>
2024-06-05 21:01:37 +0200

    `bin/rails db:migrate`

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_05_185921) do
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end

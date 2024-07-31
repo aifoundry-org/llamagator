533c4d7a469c329dce8464573043f082afc3588f
Vadser <vadser1999@gmail.com>
2024-07-31 12:28:39 +0200

    Change enum values from array to hash

--- app/models/test_result.rb
+++ app/models/test_result.rb
@@ -4,7 +4,7 @@ class TestResult < ApplicationRecord
   belongs_to :test_model_version_run
   has_many :assertion_results, dependent: :destroy
 
-  enum :status, %i[pending completed failed], scopes: true, default: :pending
+  enum :status, { pending: 0, completed: 1, failed: 2 }, scopes: true, default: :pending
 
   delegate :model_version, to: :test_model_version_run
 

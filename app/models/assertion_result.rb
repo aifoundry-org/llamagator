7204c4a9a2dbed6eda1a1b5e36b10dda79e70fe5
vadser <vadser1999@gmail.com>
2024-07-31 12:30:14 +0200

    Change enum values from array to hash (#44)

--- app/models/assertion_result.rb
+++ app/models/assertion_result.rb
@@ -3,7 +3,7 @@
 class AssertionResult < ApplicationRecord
   belongs_to :assertion
   belongs_to :test_result
-  enum :state, %i[passed failed], scopes: true
+  enum :state, { passed: 0, failed: 1 }, scopes: true
 
   default_scope { order(id: :desc) }
 end

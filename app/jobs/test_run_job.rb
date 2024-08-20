2c8639a5b2d8c4c6de382a82014cf385fba1ce5e
White Rabbit <wwwwhiterabbit@proton.me>
2024-08-20 11:43:49 +0400

    bulk job enqueuing

--- app/jobs/test_run_job.rb
+++ app/jobs/test_run_job.rb
@@ -4,10 +4,10 @@ class TestRunJob < ApplicationJob
   def perform(test_run_id)
     test_run = TestRun.find(test_run_id)
 
-    test_run.test_model_version_run_ids.each do |test_model_version_run_id|
-      test_run.calls.times do
-        TestModelVersionRunJob.perform_later(test_model_version_run_id)
-      end
+    jobs = test_run.test_model_version_run_ids.flat_map do |test_model_version_run_id|
+      Array.new(test_run.calls) { TestModelVersionRunJob.new(test_model_version_run_id) }
     end
+
+    ActiveJob.perform_all_later(jobs)
   end
 end

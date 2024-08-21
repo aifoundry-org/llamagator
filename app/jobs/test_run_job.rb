5abd8c25d446ec86232a8d7ee9b3be2a3dcfa6b8
White Rabbit <wwwwhiterabbit@proton.me>
2024-08-21 14:22:16 +0400

    remove unnecessary transaction

--- app/jobs/test_run_job.rb
+++ app/jobs/test_run_job.rb
@@ -2,12 +2,10 @@
 
 class TestRunJob < ApplicationJob
   def perform(test_run_id)
-    ApplicationRecord.transaction do
-      test_run = TestRun.find(test_run_id)
+    test_run = TestRun.find(test_run_id)
 
-      test_run.test_model_version_run_ids.each do |test_model_version_run_id|
-        PerformTestModelVersionRunJobs.new(test_run, test_model_version_run_id).call
-      end
+    test_run.test_model_version_run_ids.each do |test_model_version_run_id|
+      PerformTestModelVersionRunJobs.new(test_run, test_model_version_run_id).call
     end
   end
 end

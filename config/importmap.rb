5c8bd30787c4fea3744ad27d538486a1ad5027cb
Vadser <vadser1999@gmail.com>
2024-08-28 11:07:38 +0200

    Improve compare page js

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,6 +7,5 @@ pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin 'init-alpine', to: 'init-alpine.js', preload: true
-pin 'compare', to: 'compare.js', preload: true
 pin 'focus-trap.js', to: 'focus-trap.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers', preload: true
